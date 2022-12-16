df9d0aa8c1a51200e00f7938da8a67c32823b8fe
Liz Griffin <liz@spiritai.com>
2022-12-16 11:29:33 +0000

    Progress on day 4 part 2

--- day-4/day-4.go
+++ day-4/day-4.go
@@ -1,9 +1,47 @@
 package main
 
 import (
-	"fmt"
 	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
-func main() {
 
+func main() {
+	file, _ := os.Open("day-4/assignment-pairs.txt")
+	defer file.Close()
+
+	overlapCountPart1, overlapCountPart2 := 0, 0
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		splitLine := strings.Split(line, ",")
+		firstElf, secondElf := splitLine[0], splitLine[1]
+		firstElfStart, firstElfEnd := strings.Split(firstElf, "-")[0], strings.Split(firstElf, "-")[1]
+		secondElfStart, secondElfEnd := strings.Split(secondElf, "-")[0], strings.Split(secondElf, "-")[1]
+
+		firstElfStartInt, _ := strconv.ParseInt(firstElfStart, 10, 0)
+		firstElfEndInt, _ := strconv.ParseInt(firstElfEnd, 10, 0)
+		secondElfStartInt, _ := strconv.ParseInt(secondElfStart, 10, 0)
+		secondElfEndInt, _ := strconv.ParseInt(secondElfEnd, 10, 0)
+
+		if (secondElfStartInt >= firstElfStartInt && secondElfEndInt <= firstElfEndInt) ||
+			(firstElfStartInt >= secondElfStartInt && firstElfEndInt <= secondElfEndInt) {
+			overlapCountPart1 = overlapCountPart1 + 1
+		}
+
+		// (firstS <= secondS and firstE => secondE)
+		// (firstS => secondS and firstE <= secondE)
+		if (secondElfStartInt >= firstElfStartInt && secondElfEndInt <= firstElfEndInt) ||
+			(firstElfStartInt >= secondElfStartInt && firstElfEndInt <= secondElfEndInt) ||
+			(firstElfStartInt == secondElfEndInt || firstElfEndInt == secondElfStartInt) {
+			fmt.Print("overlap 2: ", firstElf, " --- ", secondElf, "\n")
+			overlapCountPart2 = overlapCountPart2 + 1
+		}
+
+	}
+	fmt.Print("total overlap count partido uno: ", overlapCountPart1, "\n")
+	fmt.Print("total overlap count partido dos: ", overlapCountPart2, "\n")
 }
